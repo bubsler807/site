8862752c61222d663d73d2f53fc7bfd679dcc9df
Matthew Van Den Bemd <mathewvandenbemd@gmail.com>
2025-02-25 22:26:17 -0700

    Quartz sync: Feb 25, 2025, 10:26 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -46,7 +46,7 @@ const config: QuartzConfig = {
           gray: "#646464",
           darkgray: "#d4d4d4",
           dark: "#ebebec",
-          secondary: "#ffed96",
+          secondary: "#ffb566",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
           textHighlight: "#b3aa0288",
