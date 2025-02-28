ed4d92ffb41d60bf0dc5a5d104fe86a2601a370e
Matthew Van Den Bemd <mathewvandenbemd@gmail.com>
2025-02-27 22:38:11 -0700

    Quartz sync: Feb 27, 2025, 10:38 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -58,7 +58,7 @@ const config: QuartzConfig = {
     transformers: [
       Plugin.FrontMatter(),
       Plugin.CreatedModifiedDate({
-        priority: ["git", "frontmatter", "filesystem"],
+        priority: ["filesystem", "frontmatter"],
       }),
       Plugin.SyntaxHighlighting({
         theme: {
