4b570e17e106599c1767592a40648ee34e343cd0
Matthew Van Den Bemd <mathewvandenbemd@gmail.com>
2025-03-05 22:59:42 -0700

    Quartz sync: Mar 5, 2025, 10:59 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://bubsler807.github.io/site/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
